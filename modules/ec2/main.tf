91c68d9340b2d36f7891a0817be9311313927cdf
ravn-marioRosales11 <mariorosales@ravn.co>
2025-10-07 13:14:32 -0600

    added new tasks to notes

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "ec2" {
   instance_type               = var.instance_type
   subnet_id                   = var.subnet_id
   vpc_security_group_ids      = [var.sg_id]
-  associate_public_ip_address = true
+  associate_public_ip_address = true # NOTE: for private ec2 instance, this is not required.
 
   #Script de inicialización
   user_data = file("${path.module}/user_data_amazonlinux.sh")
@@ -15,3 +15,10 @@ resource "aws_instance" "ec2" {
   }
 }
 
+# resource "aws_instance" "bastion_host" {
+#   count = var.bastion_host ? 1 : 0
+#   ami = "algo"
+#   subnet_id = "algo"
+#   instance_type = "algo"
+#   associate_public_ip_address = "algo"
+# }
